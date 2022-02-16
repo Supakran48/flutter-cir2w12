4756e8a938d919038e53fed500a88b89daef0d09
Supakran48 <69299126+Supakran48@users.noreply.github.com>
2022-02-16 18:41:04 +0700

    Add files via upload

---
+++ flutter_cir12/lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'pages/grid_view_dynamic.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter App',
+      home: GridViewDynamic(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
